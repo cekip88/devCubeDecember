0a33e04d346d8f71866ea97a9fe740ad8904d44a
cekip88 <cekip@mail.ru>
2021-12-13 11:29:02 +0600

    Start of works

---
+++ dev/js/front.js
@@ -0,0 +1,16 @@
+import { MainEventBus } from "./libs/MainEventBus.lib.js";
+import { _front } from "./libs/_front.js";
+class Front extends _front{
+  constructor(){
+    super();
+    const _ = this;
+    MainEventBus
+      .on(_,'createOrderSuccess')
+      .on(_,'createOrderFail');
+  }
+  createOrderSuccess(orderData){}
+  createOrderFail(orderData){}
+
+
+}
+new Front();
