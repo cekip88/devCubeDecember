480ee23420b444a0653188e3fd2be5f44baa755b
cekip88 <cekip@mail.ru>
2021-12-13 17:39:58 +0600

    5 blocks

--- dev/js/front.js
+++ dev/js/front.js
@@ -5,12 +5,48 @@ class Front extends _front{
     super();
     const _ = this;
     MainEventBus
+      .on(_,'burgerClick')
+      .on(_,'hireUs')
       .on(_,'createOrderSuccess')
       .on(_,'createOrderFail');
   }
   createOrderSuccess(orderData){}
   createOrderFail(orderData){}
 
+  headScroll(){
+    let head = document.querySelector('.head');
+    if (window.pageYOffset) {
+      head.classList.add('scrolled')
+    } else {
+      head.classList.remove('scrolled')
+    }
+    window.addEventListener('scroll',()=>{
+      if (window.pageYOffset) {
+        head.classList.add('scrolled')
+      } else {
+        head.classList.remove('scrolled')
+      }
+    })
+  }
 
+  burgerClick(){
+    let head = document.querySelector('.head');
+    if (head.classList.contains('active')) {
+      head.classList.remove('active');
+      document.body.removeAttribute('style');
+    } else {
+      head.classList.add('active');
+      document.body.style = 'overflow:hidden;'
+    }
+  }
+  hireUs(clickData){
+    const _ = this;
+    console.log(clickData)
+  }
+
+  init(){
+    const _ = this;
+    _.headScroll();
+  }
 }
 new Front();
